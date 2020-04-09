4d4eb638e82a2af0a1c19b7077c797aa8f196f4b
Vojta Stavik <stavik@outlook.com>
2020-04-09 16:38:31 +0200

    Add example subprojects

---
+++ Subproject1/Subproject1/Subproject1.h
@@ -0,0 +1,19 @@
+//
+//  Subproject1.h
+//  Subproject1
+//
+//  Created by Vojta on 09/04/2020.
+//  Copyright © 2020 Industrial Binaries. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Subproject1.
+FOUNDATION_EXPORT double Subproject1VersionNumber;
+
+//! Project version string for Subproject1.
+FOUNDATION_EXPORT const unsigned char Subproject1VersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Subproject1/PublicHeader.h>
+
+
---
+++ Subproject2/Subproject2/Subproject2.h
@@ -0,0 +1,19 @@
+//
+//  Subproject2.h
+//  Subproject2
+//
+//  Created by Vojta on 09/04/2020.
+//  Copyright © 2020 Industrial Binaries. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Subproject2.
+FOUNDATION_EXPORT double Subproject2VersionNumber;
+
+//! Project version string for Subproject2.
+FOUNDATION_EXPORT const unsigned char Subproject2VersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Subproject2/PublicHeader.h>
+
+
